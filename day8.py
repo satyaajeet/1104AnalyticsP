00575cf22de4539ec6027b1d38fd61047663ed53
vikaskhullar <vikas.khullar@gmail.com>
2022-04-21 22:08:37 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -5,3 +5,16 @@ Created on Thu Apr 21 21:33:32 2022
 @author: vikas
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+x = np.array([5,15,25,35,45,55])
+y = np.array([5,20,14,32,22,38])  #1 dim
+
+x
+y
+
+plt.scatter(x,y)
+
+from sklearn.linear_model import LinearRegression
